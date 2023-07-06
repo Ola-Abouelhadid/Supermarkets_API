1d0b83dc0d270a922a5787c6dc1db2a948a85e2f
Ola Abouelhadid <abouelhadid.ola@gmail.com>
2023-07-06 21:44:06 +0300

    Initial setup for files, maven, and dependencies.

---
+++ SupermarketAPI/src/main/java/com/Segmatek/OlaAbouelhadid/SupermarketAPI/SupermarketApiApplication.java
@@ -0,0 +1,13 @@
+package com.Segmatek.OlaAbouelhadid.SupermarketAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SupermarketApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SupermarketApiApplication.class, args);
+	}
+
+}
---
+++ SupermarketAPI/src/test/java/com/Segmatek/OlaAbouelhadid/SupermarketAPI/SupermarketApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Segmatek.OlaAbouelhadid.SupermarketAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SupermarketApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
