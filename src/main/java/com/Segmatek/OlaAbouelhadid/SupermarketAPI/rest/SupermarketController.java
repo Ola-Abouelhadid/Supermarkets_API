5b87cf71f97f9095d30a1ccfd16fdf51d08b6531
Ola Abouelhadid <abouelhadid.ola@gmail.com>
2023-07-07 22:19:51 +0300

    Architecture

---
+++ src/main/java/com/Segmatek/OlaAbouelhadid/SupermarketAPI/rest/SupermarketController.java
@@ -0,0 +1,6 @@
+package com.Segmatek.OlaAbouelhadid.SupermarketAPI.rest;
+
+public class SupermarketController {
+
+    // will define here all the endpoints for the API
+}
---
+++ src/main/java/com/Segmatek/OlaAbouelhadid/SupermarketAPI/service/SupermarketService.java
@@ -0,0 +1,5 @@
+package com.Segmatek.OlaAbouelhadid.SupermarketAPI.service;
+
+public interface SupermarketService {
+    //define here all the functions that will be used in the controller
+}
